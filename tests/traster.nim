7df49370afb25475d51a489abed9555b6d58f59c
Paul Norrie <paul.m.norrie@gmailcom>
2022-01-11 22:05:52 +1300

    added test

--- tests/traster.nim
+++ tests/traster.nim
@@ -2,7 +2,12 @@ import std/[unittest]
 import geomap/geomap, geomap/raster
 
 test "readRaster beyond bounds raises IOError":
+  let map = geomap.open("testdata/geoRGB.tiff")
+      
   expect IOError:
-      let map = geomap.open("testdata/geoRGB.tiff")
-      let raster = map.readRaster(BIP, x = 200, y = 200, width = 1, height = 1, [])
-      check raster.data.len == 0
+    # x, y out of bounds
+    discard map.readRaster(BIP, x = 200, y = 200, width = 1, height = 1, [])
+  
+  expect IOError:
+    # x, y in bounds but x+width, y+height are not
+    discard map.readRaster(BIP, x = 1, y = 1, width = 100, height = 100, [])
